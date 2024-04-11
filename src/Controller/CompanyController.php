35720e476619e705603de0e1e313ef3856f4c8d8
Dilson <dilsono316@gmail.com>
2024-04-11 09:15:15 -0300

    store company

---
+++ src/Controller/CompanyController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Company;
+use App\Repository\CompanyRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CompanyController extends AbstractController
+{
+    #[Route('/companies', name: 'companies', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/CompanyController.php',
+        ]);
+    }
+
+    #[Route('/companies', name: 'companies_store', methods: ['POST'])]
+    public function store(Request $request, CompanyRepository $companyRepository): JsonResponse
+    {
+        $data = $request->request->all();
+        $company = new Company();
+        $company->setName($data['name']);
+        $company->setCnpj($data['cnpj']);
+        $company->setSite($data['site']);
+        $company->setPhone($data['phone']);
+        $company->setCreatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+
+        $companyRepository->add($company, true);
+
+        return $this->json([
+            'message' => 'Company Created successfuly',
+            'path' => 'src/Controller/CompanyController.php',
+        ], 201);
+    }
+}
